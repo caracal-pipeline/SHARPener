21a77cb4c26d47b1b6434ce4730ea7d438453252
Robert Schulz <schulz@astron.nl>
2020-01-31 14:07:49 +0100

    added peak flux to title

--- sharpener/sharp_modules/absorption_plot.py
+++ sharpener/sharp_modules/absorption_plot.py
@@ -311,6 +311,10 @@ def abs_plot(spec_name, cfg_par):
     rc('font', **{'family': 'serif', 'serif': ['serif']})
     plt.rcParams.update(params)
 
+    # get the parameters of the continuum sources
+    radio_src_cat_file = "{0:s}abs/mir_src_sharp.csv".format(sharpDir)
+    radio_src_cat = Table.read(radio_src_cat_file, format='csv')
+
     # for i in xrange(0,len(np.atleast_1d(spec_src_name))):
 
     # load data and labels
@@ -417,9 +421,11 @@ def abs_plot(spec_name, cfg_par):
                              y1_max, facecolor='red', alpha=0.1)
 
         # Add title
+        src_id = int(os.path.basename(spec_name).split('_')[0])
+        src_continuum_peak = radio_src_cat[np.where(radio_src_cat["ID"]==src_id)]['Peak'][0]
         if cfg_par[key]['title'] == True:
-            ax1.set_title("{0:s} (\#{1:d}): {2:s}".format(cfg_par['general']['label'], int(os.path.basename(spec_name).split('_')[0]), os.path.basename(spec_name).replace(
-                '.txt', '').split('_')[-1]), fontsize=font_size+2)
+            ax1.set_title("{0:s} (\#{1}): {2:s} (peak {3:.1f}mJy/beam)".format(cfg_par['general']['label'], src_id, os.path.basename(spec_name).replace(
+                '.txt', '').split('_')[-1], src_continuum_peak*1.e3), fontsize=font_size+2)
             # if self.abs_ex_plot_title == True:
         #	ax1.set_title('%s' % (self.J2000_name[i]), fontsize=font_size+2)
         # ax1.axes.titlepad = 8
